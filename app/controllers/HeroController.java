0bb673519b4e1b0e1f45d24d7bf346454f976e3c
Dong Phung Trong <dong.vtcgd@gmail.com>
2018-11-19 11:05:47 +0700

    Project

---
+++ app/controllers/HeroController.java
@@ -0,0 +1,49 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import models.Hero;
+import play.mvc.Controller;
+import play.mvc.Result;
+import services.Counter;
+
+/**
+ * This controller demonstrates how to use dependency injection to
+ * bind a component into a controller class. The class contains an
+ * action that shows an incrementing count to users. The {@link Counter}
+ * object is injected by the Guice dependency injection system.
+ */
+@Singleton
+public class HeroController extends Controller {
+
+    private List<Hero> heroes;
+
+    @Inject
+    public HeroController() {
+        this.heroes = new ArrayList<>();
+    }
+
+
+
+    public Result create() {
+        Hero hero = play.libs.Json.fromJson(ctx().request().body().asJson(), Hero.class);
+        if(hero.getLevel() < 1) {
+            return badRequest();
+        } else {
+            heroes.add(hero);
+            return created();
+        }
+    }
+
+    public Result get() {
+        return ok(play.libs.Json.toJson(heroes));
+    }
+
+    public Result list() {
+        return ok(views.html.heroList.render(heroes));
+    }
+
+
+}
---
+++ app/models/Hero.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Hero {
+  private int level;
+  private int hp;
+
+  public int getLevel() {
+    return level;
+  }
+
+  public void setLevel(int level) {
+    this.level = level;
+  }
+
+  public int getHp() {
+    return hp;
+  }
+
+  public void setHp(int hp) {
+    this.hp = hp;
+  }
+}
